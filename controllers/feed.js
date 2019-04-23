3a16e895cf835747ae0225aa0e00811ef2e7693b
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-04-23 22:29:55 +0200

    Fetching a singel post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,16 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post',
-                content: 'This is the first post!',
-                imageUrl: 'images/js.jpg',
-                creator: {
-                    name: 'Michael'
-                },
-                createdAt: new Date()
+    Post.find()
+        .then(posts => {
+            res.status(200).json({message: 'Ferched post successfully.', posts})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            next(err);
+        })
 };
 
 exports.createPost = (req, res, next) => {
@@ -46,5 +42,24 @@ exports.createPost = (req, res, next) => {
         }
         next(err) // in async you have to use next and pass err => middleware error in app.js
     })
-
 };
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if(!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error //reminder :) for this case you don't have to use next
+                            // because data is going to catch and there is next
+            }
+            res.status(200).json({ message: 'Post fetched', post})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err) // in async you have to use next and pass err => middleware error in app.js
+        })
+}
