a2298f15c4e66decc686287844b1badbf4591d03
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-05-12 16:16:41 +0200

    adding auth middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,7 @@ exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
-    User.findOne({email: email})
+    User.findOne({ email: email })
         .then(user => {
             if (!user) {
                 const error = new Error('A user with this email could not be found.');
@@ -52,14 +52,16 @@ exports.login = (req, res, next) => {
         })
         .then(isEqual => {
             if (!isEqual) {
-               const error = new Error('Wrong password!');
-               error.statusCode = 401;
-               throw error;
+                const error = new Error('Wrong password!');
+                error.statusCode = 401;
+                throw error;
             }
-            const token = jwt.sign({
-                email: loadedUser.email,
-                userId: loadedUser._id.toString()
-            }, 'somesupersecretsecret',
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'somesupersecretsecret',
                 { expiresIn: '1h' }
             );
             res.status(200).json({ token: token, userId: loadedUser._id.toString() });
@@ -69,5 +71,5 @@ exports.login = (req, res, next) => {
                 err.statusCode = 500;
             }
             next(err);
-    })
-}
+        });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -2,14 +2,16 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 //GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 //POST /feed/posts
 router.post('/post',
+    isAuth,
     [
         body('title').trim().isLength({min: 5}),
         body('content').trim().isLength({min: 5}),
@@ -17,8 +19,9 @@ router.post('/post',
     feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 router.put('/post/:postId',
+    isAuth,
     [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5}),
@@ -26,6 +29,6 @@ router.put('/post/:postId',
     feedController.updatePost
 );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
