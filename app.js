50277304cf27f7faf19beea1cf0bac1b03e7c555
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-06-15 13:40:28 +0200

    Setting up Socket on the server side

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ mongoose
      'mongodb+srv://Michael:b8sSmwk7I7wz7hpf@cluster0-flk3y.mongodb.net/blog'
     )
     .then(result => {
-        app.listen(8080);
+       const server =  app.listen(8080);
+       const io = require('socket.io')(server);
+       io.on('connection', socket => {
+           console.log('Client connected');
+       });
     }).catch(err => console.log(err));
 
